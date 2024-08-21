9461a2c3d7bf6a10da68a826ecbf464056c64563
Vellu <veli-matti.heikkila@helsinki.fi>
2024-08-21 10:07:28 +0300

    deploy db to azure

---
+++ init.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Copy necessary files from the mounted volume
+cp /shib-config/shibboleth2.xml /etc/shibboleth/shibboleth2.xml
+cp /shib-config/attribute-map.xml /etc/shibboleth/attribute-map.xml

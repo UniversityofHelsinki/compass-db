dcbfb9883695ab1d0d00a6aa818ffca58a304056
Vellu <veli-matti.heikkila@helsinki.fi>
2024-09-03 17:06:54 +0300

    added hello world logging

--- api/routes.js
+++ api/routes.js
@@ -2,6 +2,7 @@ import {logger} from "../logger.js";
 
 const router = (router) => {
     router.get('/hello', (req, res) => {
+        logger.info('hello world');
         res.json({message: 'Hello, world!'});
     });
 };

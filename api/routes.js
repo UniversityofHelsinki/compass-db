5154bb6f4a643f48a16b25dcd98c94f2563c22c9
anttilan <antti.lankinen@helsinki.fi>
2024-09-04 13:18:24 +0300

    Vastausten tallennus, pahasti kesken.

--- api/routes.js
+++ api/routes.js
@@ -2,6 +2,11 @@ const router = (router) => {
     router.get('/hello', (req, res) => {
         res.json({message: 'Hello, world!'});
     });
+
+    router.post('/saveanswer', (req, res) => {
+        let answer = req.body;
+        res.json([{message: 'Answer stored!'}]);
+    });
 };
 
 export default router;

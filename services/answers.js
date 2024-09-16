51436168e7a673fd9d8227e53d3c9d8fb6f77fd4
anttilan <antti.lankinen@helsinki.fi>
2024-09-16 12:11:00 +0300

    Virheenkäsittelyä.

--- services/answers.js
+++ services/answers.js
@@ -1,14 +1,20 @@
 const dbApi = require ("../api/dbApi.js");
 const {logger} = require("../logger");
+const messageKeys = require('../utils/message-keys');
 
  exports.insertanswer = async (req, res) => {
     try {
         let answer = req.body;
         await dbApi.insertanswer(answer);
         logger.info(`Answer stored`)
-        res.json([{message: 'Answer stored'}]);
+        res.json([{message: messageKeys.MESSAGE_ANSWER_SAVED}]);
     } catch (error) {
         logger.error(`error inserting answer`);
-        throw error;
+        const msg = error.message;
+        logger.error(`Error GET /userInboxEvents ${error} ${msg}  USER ${req.user.eppn}`);
+        res.status(500);
+        return res.json([{
+            message: messageKeys.ERROR_MESSAGE_FAILED_TO_SAVE_ANSWER
+        }]);
     }
 }
---
+++ utils/message-keys.js
@@ -0,0 +1,8 @@
+
+const ERROR_MESSAGE_FAILED_TO_SAVE_ANSWER = 'error-failed-to-save-answer';
+const MESSAGE_ANSWER_SAVED = 'answer-saved';
+
+module.exports = {
+    ERROR_MESSAGE_FAILED_TO_SAVE_ANSWER,
+    MESSAGE_ANSWER_SAVED
+};

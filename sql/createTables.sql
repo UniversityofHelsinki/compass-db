9f6d587c0054d3d748b20b16497f79a6f5ca5bb2
anttilan <antti.lankinen@helsinki.fi>
2024-09-19 13:06:10 +0300

    Taulujen luontijärjestys muutettu.

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -1,4 +1,22 @@
 -- creates answers table
+CREATE TABLE IF NOT EXISTS TEACHER (
+   user_id VARCHAR(8) NOT NULL UNIQUE,
+   created TIMESTAMP,
+   PRIMARY KEY(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS COURSE (
+  id integer UNIQUE NOT NULL,
+  course_id VARCHAR(255),
+  course_title VARCHAR(255),
+  course_description VARCHAR(255),
+  created TIMESTAMP,
+  start_date date,
+  end_date date,
+  teacher varchar(8) REFERENCES TEACHER (user_id),
+  PRIMARY KEY(id)
+);
+
 CREATE TABLE IF NOT EXISTS ANSWERS(
     id SERIAL,
     studentid VARCHAR(50),
@@ -16,23 +34,6 @@ CREATE TABLE IF NOT EXISTS STUDENT (
     user_id VARCHAR(8) NOT NULL unique not null,
     created TIMESTAMP,
     PRIMARY KEY(user_id)
-    );
-CREATE TABLE IF NOT EXISTS TEACHER (
-    user_id VARCHAR(8) NOT NULL UNIQUE,
-    created TIMESTAMP,
-    PRIMARY KEY(user_id)
-    );
-
-CREATE TABLE IF NOT EXISTS COURSE (
-    id integer UNIQUE NOT NULL,
-    course_id VARCHAR(255),
-    course_title VARCHAR(255),
-    course_description VARCHAR(255),
-    created TIMESTAMP,
-    start_date date,
-    end_date date,
-    teacher varchar(8) REFERENCES TEACHER (user_id),
-    PRIMARY KEY(id)
     );
 
 CREATE TABLE  IF NOT EXISTS STUDENT_COURSE (

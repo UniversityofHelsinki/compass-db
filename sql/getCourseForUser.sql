5d5f922831349d826ca7cd8af77d8551a1f716b8
jooaho <jooaho@helsinki.fi>
2024-10-07 08:34:25 +0300

    Remove userCourse

--- sql/getCourseForUser.sql
+++
@@ -1,5 +0,0 @@
-SELECT u.user_name, c.course_id, c.course_title
-FROM users u
-    JOIN user_courses uc ON u.user_name = uc.user_name
-    JOIN courses c ON uc.course_id = c.course_id
-ORDER BY u.user_name, c.course_id;

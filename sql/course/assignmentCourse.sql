8674c771c21621f1522bc86dfcffb243763e1687
anttilan <antti.lankinen@helsinki.fi>
2024-10-11 07:12:33 +0300

    assignment ja course.

--- sql/course/assignmentCourse.sql
+++ sql/course/assignmentCourse.sql
@@ -2,5 +2,5 @@ SELECT COURSE.title, assignment.topic
 FROM ASSIGNMENT
          JOIN COURSE ON
     ASSIGNMENT.course_id = COURSE.course_id AND
-    ASSIGNMENT.assignment_id = '1';
+    ASSIGNMENT.assignment_id = $1;
 

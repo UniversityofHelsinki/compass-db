fea03c7152acb10e9d4b2f0a459fa3c1d00e248a
anttilan <antti.lankinen@helsinki.fi>
2024-10-14 12:15:44 +0300

    assignment ja course.

--- sql/student/answerAssignmentCourse.sql
+++ sql/student/answerAssignmentCourse.sql
@@ -1,4 +1,4 @@
-SELECT course.title, assignment.topic, answer.value, answer.order_nbr
+SELECT answer.assignment_id, course.title, assignment.topic, answer.value, answer.order_nbr
 FROM course
          JOIN answer ON
     answer.course_id = course.course_id AND

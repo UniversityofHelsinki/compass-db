2b06c3e0cb39cff89b78139ddf0cb24c46fdab8a
Riku Hänninen <riku.hanninen@helsinki.fi>
2024-10-15 07:13:14 +0300

    Course creation & save

--- services/assignments.js
+++ services/assignments.js
@@ -1,5 +1,32 @@
 const database = require('../services/database.js');
 
+const columns = (assignment) => {
+  const keys = ['course_id', 'topic', 'start_date', 'end_date'];
+  const values = keys.map(key => assignment[key]);
+  if (assignment.id) {
+    return [assignment.id, ...values];
+  }
+  return values;
+};
+
+exports.save = async (assignment) => {
+  if (!assignment) {
+    throw new Error(
+      `assignment ${assignment} must be defined.`
+    );
+  }
+  return await database.execute('assignment/save.sql', columns(assignment));
+};
+
+exports.update = async (assignment) => {
+  if (!assignment) {
+    throw new Error(
+      `assignment ${assignment} must be defined.`
+    );
+  }
+  return await database.execute('assignment/update.sql', columns(assignment));
+};
+
 exports.forCourse = async (course) => {
   if (!course) {
     throw new Error(

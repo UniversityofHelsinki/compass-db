747b44b495be888e4f249a5c0a011a44995d02a4
Vellu <veli-matti.heikkila@helsinki.fi>
2024-10-15 16:46:35 +0300

    added test for course creation

--- services/courses.js
+++ services/courses.js
@@ -1,5 +1,7 @@
 const { read } = require("../sql/read");
 const database = require("../services/database.js");
+const fs = require("fs");
+const path = require("path");
 
 const columns = (course) => {
   const keys = ['course_id', 'user_name', 'title', 'description', 'start_date', 'end_date'];
@@ -11,11 +13,12 @@ const columns = (course) => {
 };
 
 exports.save = async (course) => {
-  return await database.execute('course/save.sql', columns(course));
+  const insertCourseSQL = fs.readFileSync(path.resolve(__dirname, "../sql/course/save.sql"), "utf8");
+  return await database.query(insertCourseSQL, columns(course));
 };
 
 exports.update = async (course) => {
-  return await database.execute('course/update.sql', columns(course));
+  return await database.query('course/update.sql', columns(course));
 };
 
 exports.forTeacher = async (teacher) => {

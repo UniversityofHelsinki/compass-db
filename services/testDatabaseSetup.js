677636478e1a87b45979eaaea61267eed95b1c0e
Vellu <veli-matti.heikkila@helsinki.fi>
2024-10-17 14:28:22 +0300

    implement execute function in databaseSetup for the tests to use

--- services/testDatabaseSetup.js
+++ services/testDatabaseSetup.js
@@ -2,6 +2,27 @@ const path = require("path");
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const Pool = require('pg-pool');
 const database = require("../services/database");
+const { readFile } = require('fs').promises;
+
+const execute = async (file, values) => {
+    if (!file) {
+        throw new Error('The "file" argument is required and must be a non-empty string.');
+    }
+
+    try {
+        const filePath = path.resolve(__dirname, '../sql', file);
+        const sql = await readFile(filePath, 'utf8');
+        const results = await database.query(sql, values);
+        return results;
+    } catch (error) {
+        console.error(`Error while reading SQL file ${file}: ${error.message}`);
+        throw new Error(
+            `Error while reading SQL file ${file} with values ${JSON.stringify(values)}`,
+            { cause: error }
+        );
+    }
+};
+
 
 const setupDatabase = async () => {
     let pool = new Pool({
@@ -17,6 +38,7 @@ const setupDatabase = async () => {
 
     database.end = () => pool.end();
     database.query = (text, values) => pool.query(text, values);
+    database.execute = execute;
 };
 
 module.exports = { setupDatabase, database };

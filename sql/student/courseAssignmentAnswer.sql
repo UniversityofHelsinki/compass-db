0d061f9395daa030f2a8540569df86f0db7fb62e
anttilan <antti.lankinen@helsinki.fi>
2024-10-22 06:24:28 +0300

    sql

---
+++ sql/student/courseAssignmentAnswer.sql
@@ -0,0 +1,8 @@
+SELECT answer.assignment_id, course.title, assignment.topic, answer.course_id, answer.id
+FROM course
+         JOIN answer ON
+    answer.course_id = course.course_id AND
+    answer.user_name = $1 AND
+    answer.course_id = $2
+         JOIN assignment ON
+    assignment.assignment_id = answer.assignment_id;

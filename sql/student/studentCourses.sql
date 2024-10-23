47e6c9f30178230882c9f8643000f7539850b7d7
anttilan <antti.lankinen@helsinki.fi>
2024-10-23 07:00:01 +0300

    sql

--- sql/student/studentCourses.sql
+++ sql/student/studentCourses.sql
@@ -1,8 +1,6 @@
 SELECT DISTINCT course.* FROM course
-                                  JOIN answer ON
+        JOIN answer ON
     answer.course_id = course.course_id AND
     answer.user_name = $1
-                                  JOIN assignment ON
+        JOIN assignment ON
     answer.assignment_id = assignment.id
-
-

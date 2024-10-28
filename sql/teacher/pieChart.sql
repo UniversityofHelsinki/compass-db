91f8ed2e09d5c4b2265b449552f2628007f89ea4
Vellu <veli-matti.heikkila@helsinki.fi>
2024-10-28 16:15:45 +0200

    modified sql

--- sql/teacher/pieChart.sql
+++ sql/teacher/pieChart.sql
@@ -1,17 +1,19 @@
 SELECT
     a.id AS assignment_id,
     c.course_id,
-    c.title AS course_title,
     a.topic AS assignment_topic,
-    AVG(ans.order_nbr) AS average_score,
+    ans.order_nbr,
+    (COUNT(ans.id) * 100.0 / SUM(COUNT(ans.id)) OVER (PARTITION BY a.id)) AS order_nbr_percentage,
     COUNT(ans.id) AS answer_count
 FROM
     COURSE c
-        JOIN ASSIGNMENT a ON c.course_id = a.course_id
-        JOIN ANSWER ans ON a.id = ans.assignment_id
+        JOIN
+    ASSIGNMENT a ON c.course_id = a.course_id
+        JOIN
+    ANSWER ans ON a.id = ans.assignment_id
 WHERE
     c.id = $1::integer
 GROUP BY
-    a.id, c.course_id, c.title, a.topic
+    a.id, c.course_id, c.title, a.topic, ans.order_nbr
 ORDER BY
     a.id;

ad069ee5911a4265082905c6c9726f8b2d352d18
anttilan <antti.lankinen@helsinki.fi>
2024-11-25 09:03:13 +0200

    Opiskelijaa ei saa liittää päättyneelle - eikä kurssille, joka ei ole vielä alkanut.

--- Constants.js
+++ Constants.js
@@ -1,3 +1,3 @@
-export const COURSE_ONGOING = 'course_ongoing';
-export const COURSE_ENDED = 'course_ended';
-export const COURSE_IN_FUTURE = 'course_in_future';
+const COURSE_ONGOING = 'course_ongoing';
+const COURSE_ENDED = 'course_ended';
+const COURSE_IN_FUTURE = 'course_in_future';
